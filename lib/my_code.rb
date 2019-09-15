4c279daa28d7c5a8b82a06a1c47438158aceba09
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-09-15 01:02:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,12 +21,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.each do
-    if (source_array[i] == false)
-      return false
-    else 
-  source_array.reduce(:+)
+
 end
 
 def reduce_to_any_true(source_array)
